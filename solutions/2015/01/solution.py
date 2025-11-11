103ba135571e63070824f9e370f8e5bcd43f2508
Keegan Brown <keeganbrown@outlook.de>
2025-11-11 16:58:48 +0100

    feat(2025/01): add initial implementation

---
+++ solutions/2015/01/solution.py
@@ -0,0 +1,29 @@
+import sys
+
+def main(input: str):
+    print(part1(input))
+    print(part2(input))
+
+def part1(input: str) -> str:
+    floor = 0
+    for c in input:
+        if c == "(":
+            floor += 1
+        elif c == ")":
+            floor -= 1
+    return str(floor)
+
+
+def part2(input: str) -> str:
+    floor = 0
+    for i, c in enumerate(input, 1):
+        if c == "(":
+            floor += 1
+        elif c == ")":
+            floor -= 1
+        if floor == -1:
+            return str(i)
+    raise RuntimeError("did not reach basement")
+
+if __name__ == "__main__":
+    main(sys.stdin.read())
